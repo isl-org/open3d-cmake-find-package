56be14f2fd89e5fe3151c86397a2defefe4ae9c5
Patrick Stotko <stotko@cs.uni-bonn.de>
2021-04-28 15:03:29 +0200

    Simplify instructions and example

--- Draw.cpp
+++ Draw.cpp
@@ -29,19 +29,17 @@
 #include "open3d/Open3D.h"
 
 int main(int argc, char *argv[]) {
-    using namespace open3d;
-
     if (argc == 2) {
         std::string option(argv[1]);
         if (option == "--skip-for-unit-test") {
-            utility::LogInfo("Skiped for unit test.");
+            open3d::utility::LogInfo("Skiped for unit test.");
             return 0;
         }
     }
 
-    auto sphere = geometry::TriangleMesh::CreateSphere(1.0);
+    auto sphere = open3d::geometry::TriangleMesh::CreateSphere(1.0);
     sphere->ComputeVertexNormals();
     sphere->PaintUniformColor({0.0, 1.0, 0.0});
-    visualization::DrawGeometries({sphere});
+    open3d::visualization::DrawGeometries({sphere});
     return 0;
 }
